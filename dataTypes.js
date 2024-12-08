2359246dfe4c5a744582ace4ee27bb95f2362429
Imranmazhar323 <imranmazhar447@gmail.com>
2024-12-09 02:26:58 +0500

    dataTypes

---
+++ dataTypes.js
@@ -0,0 +1,45 @@
+"use strict"; // treat all the js code as a newer version (it's not necessary in today's becuase every browser nows understand the js version)
+// alert(3 + 3); we are using node js, not browser
+
+// In this section readability is discussed, clean and formated code ( prettie is example )
+console.log("imran");
+console.log(3 + 3);
+
+// original documentaion for javascript website  https://tc39.es/ecma262/
+
+let name = "imran"; // string
+let age = 0; // number
+let isLoggedIn = false; // boolean
+let state; //undefined ( it means variable is defined but value is assigned)
+
+// these are premitive datatypes
+// number => 9
+// string => ""
+// bigInt  (used in trading and bg companies like facebook)
+// boolean => true/false
+// null => standalone value
+// undefinded
+// symbol => unique
+
+// these are non premitive datatypes
+// object
+
+console.log(typeof age);
+console.log(typeof null);
+
+// JavaScript has 8 Datatypes
+// String
+// Number
+// Bigint
+// Boolean
+// Undefined
+// Null
+// Symbol
+// Object
+
+// The Object Datatype
+// The object data type can contain both built-in objects, and user defined objects:
+
+// Built-in object types can be:
+
+// objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more
